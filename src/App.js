6ccd7faa082a6e8beba87f925077f25a449b88bd
Jessie Gayeon Ko <gayeonko@hotmail.com>
2019-05-16 19:52:04 -0400

    react crud

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component }from 'react';
-import './App.css';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import Subject from './components/Subject';
+import Control from './components/Control';
 import TOC from './components/TOC';
-
+import './App.css';
 
 class App extends Component{
   constructor(props){
     super(props);
+    this.max_content_id = 3; //not related to UI 
     this.state = {
-      mode: 'read',
+      mode: 'welcome',
       selected_content_id: 2,
       subject: {title: 'WEB', sub: 'world wide web!'},
       welcome: {tilte: 'Welcome', desc: 'Hello, React!!'},
@@ -20,21 +23,53 @@ class App extends Component{
       ]
     }
   }
-  render(){
-    let _title, _desc = null;
+  getReadContent(){
+    for(let i=0; i<this.state.contents.length; ++i){
+      let data = this.state.contents[i];   
+      if(data.id === this.state.selected_content_id){
+        return data;
+      }
+    }
+  }
+  getContent(){
+    let _title, _desc, _article, _content = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.tilte;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode === 'read'){
-      for(let i=0; i<this.state.contents.length; ++i){
-        let data = this.state.contents[i];   
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-      }
+      _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        //add content to this.state.contents
+        this.max_content_id += 1;
+        // let _contents = this.state.contents.concat(
+        //   {id: this.max_content_id, title: _title, desc: _desc}
+        // );
+        let _contents = Array.from(this.state.contents);
+        _contents.push({id: this.max_content_id, title: _title, desc: _desc});
+        this.setState({contents: _contents, mode:'read', selected_content_id: this.max_content_id});
+      }.bind(this)}></CreateContent>
+    } else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent  data={_content} onSubmit={
+        function(_id, _title, _desc){
+          let _contents = Array.from(this.state.contents);
+          for(let i=0; i<_contents.length; ++i){
+            if(_contents[i].id === _id){
+              _contents[i] = {id: _id, title: _title, desc: _desc};
+              break;
+            }
+          }
+          this.setState({contents: _contents, mode: 'read'});
+        }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+
+  render(){
+    console.log('App render');
     return (
       <div className="App">
         <Subject 
@@ -53,7 +88,24 @@ class App extends Component{
           }.bind(this)} 
           data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function(_mode){
+          if(_mode === 'delete'){
+            if(window.confirm('really?')){
+              let _contents = Array.from(this.state.contents);
+              for(let i=0; i<_contents.length; i++){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i, 1);
+                  break;
+                }
+              }
+              this.setState({mode: 'welcome', contents: _contents});
+              alert('deleted!!');
+            }
+          }else {
+            this.setState({mode: _mode});
+          }
+        }.bind(this)}></Control>
+        {this.getContent()}
       </div>
     );
   }
